cdbc0f18ffff31f02547bee88e126a13f0c01ff8
st10294145@vcconnect.edu.za <ST10294145@vcconnect.edu.za>
2024-06-27 02:55:19 +0200

    Created format for recipes.XAML and updated class for Recipes.XAML.cs

--- Part3/Create_Recipe.xaml.cs
+++ Part3/Create_Recipe.xaml.cs
@@ -2,9 +2,6 @@
 
 namespace Part3
 {
-    /// <summary>
-    /// Interaction logic for Create_Recipe.xaml
-    /// </summary>
     public partial class Create_Recipe : Window
     {
         private Storage _storage;
@@ -13,16 +10,18 @@ namespace Part3
         {
             InitializeComponent();
             _storage = storage;
-
-            // Use the storage object as needed
-            // For example, you could display the recipe details in the UI
-            // You can access the properties using _storage.Name, _storage.Ingredients, etc.
         }
 
-        private void btnYes_Click(object sender, RoutedEventArgs e)
+        private void btnSave_Click(object sender, RoutedEventArgs e)
         {
-            // If the user presses yes a message box with the title "WARNING!" will appear telling them it could be dangerous
-            MessageBox.Show("Anything above 300 calories can be dangerous to your health!", "WARNING!");
+            _storage.Name = txtName.Text;
+            _storage.Ingredients = txtIngredients.Text;
+            _storage.Steps = txtSteps.Text;
+            _storage.FoodGroups = txtFoodGroups.Text;
+
+            Recipes recipesWindow = new Recipes(_storage);
+            recipesWindow.Show();
+            this.Close();
         }
     }
 }
--- Part3/Recipes.xaml.cs
+++ Part3/Recipes.xaml.cs
@@ -1,27 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Part3
 {
-    /// <summary>
-    /// Interaction logic for Recipes.xaml
-    /// </summary>
     public partial class Recipes : Window
     {
-        public Recipes()
+        private Storage _storage;
+
+        public Recipes(Storage storage)
         {
             InitializeComponent();
+            _storage = storage;
+            DisplayRecipe();
+        }
+
+        private void DisplayRecipe()
+        {
+            txtName.Text = $"Name: {_storage.Name}";
+            txtIngredients.Text = $"Ingredients: {_storage.Ingredients}";
+            txtSteps.Text = $"Steps: {_storage.Steps}";
+            txtFoodGroups.Text = $"Food Groups: {_storage.FoodGroups}";
         }
     }
 }
