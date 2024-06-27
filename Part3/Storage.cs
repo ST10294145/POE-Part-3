7ee6a9ce4766f957ec43ea853913dfb994ef0f62
st10294145@vcconnect.edu.za <ST10294145@vcconnect.edu.za>
2024-06-27 02:30:35 +0200

    Added a class for whwre the recipe Name, Ingredients, Steps and, Food Groups will be stored.

---
+++ Part3/Storage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part3
+{
+    public class Storage
+    {
+        public string Name { get; set; }
+        public string Ingredients { get; set; }
+        public string Steps { get; set; }
+        public string FoodGroups { get; set; }
+
+    }
+}
